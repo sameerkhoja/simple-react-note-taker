d2e1a788f5403f30a5e6ceeab4fdf4d37161e714
Sameer Khoja <sk2327@cornell.edu>
2016-07-18 12:50:06 -0500

    more progress on tut

--- public/javascripts/Note.js
+++ public/javascripts/Note.js
@@ -1,11 +1,19 @@
 var Note = React.createClass({
   edit: function(){
-    alert('editing note');
+    this.setState({editing: true});
   },
   remove: function(){
     alert("removing note");
   },
-  render: function(){
+  save: function(){
+      var val = this.refs.newText.getDOMNode().value;
+      alert("TODO: save note value" + val);
+     this.setState({editing: false});
+  },
+  getInitialState: function(){
+    return {editing: false};
+  },
+  renderDisplay: function(){
     return (
       <div className="note">
         <p>{this.props.children}</p>
@@ -15,7 +23,62 @@ var Note = React.createClass({
         </span>
       </div>
     );
+  },
+  renderForm: function(){
+    return(
+      <div className="note">
+        <textarea ref="newText" defaultValue={this.props.children} className="form-control"></textarea>
+        <button onClick={this.save} className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk"></button>
+      </div>
+    );
+  },
+  render: function(){
+    if(this.state.editing)
+      return this.renderForm();
+    else {
+      return this.renderDisplay();
+    }
   }
 });
 
-React.render(<Note>Hello World</Note>, document.getElementById('react-container'));
+var Board = React.createClass({
+  getInitialState: function(){
+    return {
+      notes: [
+        "Call Sahil",
+        "get paper",
+        "react is dope",
+        "wtf how is this working"
+      ]
+    };
+  },
+  propTypes: {
+    count: function(props, propName){
+      if(typeof props[propName] !== "number")
+        return new Error('The count property must be a number');
+    }
+  },
+  update: function(newText, i){
+    var arr = this.state.notes;
+    arr[i] = newText;
+    this.setState({notes: arr});
+  },
+  remove: function(i){
+    var arr = this.state.notes;
+    arr.splice(i, 1);
+    this.setState({notes: arr});
+  }
+  render: function(){
+    return (
+      <div className="board">
+        {this.state.notes.map(function(note, i){
+          return(
+            <Note key={i}>{note}</Note>
+          );
+        })}
+      </div>
+    );
+  }
+})
+
+React.render(<Board count={10} />, document.getElementById('react-container'));
